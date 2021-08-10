c4c6b620c52cceede732e7c1a055b53d70d13601
Captoweb <captainphp@yandex.ru>
2021-08-11 00:28:25 +0300

    Update create.php

--- create.php
+++ create.php
@@ -3,8 +3,15 @@
 $pdo = new PDO("mysql:host=localhost;dbname=x5studio", "root", "");
 $sql = "INSERT INTO books ( name, description, pages, price) VALUES ( :name, :description, :pages, :price)";
 $stmt = $pdo->prepare($sql);
+
+$_POST['name'] = htmlspecialchars($_POST['name']);
+$_POST['description'] = htmlspecialchars($_POST['description']);
+
+ $stmt->bindParam(":name", $_POST['name']);
+ $stmt->bindParam(":description", $_POST['description']);
+// $stmt->bindParam(":pages", $_POST['pages']);
+// $stmt->bindParam(":price", $_POST['price']);
+
 $stmt->execute($_POST);
     
-
-
 header("Location: /"); exit;  
